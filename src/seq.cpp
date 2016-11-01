39abd0679d94b14e15608b33465def096702e9c5
rmnorris <rmnorris@well.ox.ac.uk>
2016-11-01 14:32:21 +0000

    modified so only work out minimizer when need to

--- src/seq.cpp
+++ src/seq.cpp
@@ -27,47 +27,68 @@ Seq::~Seq()
 
 void Seq::minimizer_sketch (uint32_t w, uint32_t k)
 {
+    //cout << "Start sketching" << endl;
     // If sequence too short, just return
     if (seq.length()+1 < w+k) {//cout << "Sequence too short to sketch" << endl; 
 	return;}
 
     // Store minimizers in vector until the end, so don't have to sort every time add a new element
     deque<Minimizer*> v;
+    // initializations
+    string kmer;
+    uint64_t kh, smallest;
+    Minimizer *m;
 
     // for each window position
     for(uint32_t wpos=0; wpos <= seq.length()-w-k+1 ; ++wpos)
     {
-	// find the lex smallest kmer in the window
-	//set<string> kmers;
-	//set<uint64_t> kmers;
-	string kmer;
-	uint64_t kh;
-	uint64_t smallest = std::numeric_limits<uint64_t>::max();
-
-	for (uint32_t i = 0; i < w; i++)
+	//cout << "wpos: " << wpos << endl;
+    	// if wpos==0 or the previous minimizer starts outside current window, calculate new mini from scratch
+        if((wpos == 0) or (v.front()->pos.start < wpos))
 	{
-	    kmer = seq.substr(wpos+i, k);
-	    kh = kmerhash(kmer, k);
-	    //kmers.insert(kh);
-	    smallest = min(smallest, kh);
-	}
-	//uint64_t smallest_word = *kmers.begin();
-	for (uint32_t i = 0; i < w; i++)
-	{
-	    kmer = seq.substr(wpos+i, k);
-	    kh = kmerhash(kmer, k);
-	    if (kh == smallest)
-            {
-            	Minimizer *m;
-		m = new Minimizer(kh, wpos+i, wpos+i+k);
- 		pointer_values_equal<Minimizer> eq = { m };
-                if ( find_if(v.begin(), v.end(), eq) == v.end() )
-		{
-		    v.push_front(m);
-		} else {
-		    delete m;
-		}
+            smallest = std::numeric_limits<uint64_t>::max();
+	    // find the lex smallest kmer in the window
+	    for (uint32_t i = 0; i < w; i++)
+	    {
+	    	kmer = seq.substr(wpos+i, k);
+	        kh = kmerhash(kmer, k);
+	        smallest = min(smallest, kh);
 	    }
+	    for (uint32_t i = 0; i < w; i++)
+	    {
+	        kmer = seq.substr(wpos+i, k);
+	        kh = kmerhash(kmer, k);
+	        if (kh == smallest)
+                {
+		    m = new Minimizer(kh, wpos+i, wpos+i+k);
+ 		    pointer_values_equal<Minimizer> eq = { m };
+                    if ( find_if(v.begin(), v.end(), eq) == v.end() )
+		    {
+			//cout << "Added minimizer " << *m << endl;
+		    	v.push_front(m);
+		    } else {
+		        delete m;
+		    }
+	        }
+	    }
+        } else {
+        // otherwise only need to do something if the kmer from the newest position at end of new window is smaller or equal to the previous smallest
+	    kmer = seq.substr(wpos+w-1, k);
+            kh = kmerhash(kmer, k);
+	    //cout << "Last kh for wpos: " << kh << " compared to previous smallest: " << smallest << endl;
+	    if(kh <= smallest)
+	    {
+	        m = new Minimizer(kh, wpos+w-1, wpos+w-1+k);
+                pointer_values_equal<Minimizer> eq = { m };
+                if ( find_if(v.begin(), v.end(), eq) == v.end() )
+                {
+		    //cout << "Added minimizer " << *m << endl;
+                    v.push_front(m);
+                } else {
+                    delete m;
+                }
+       	    }
+            smallest = min(smallest, kh);
 	}
     }
     
