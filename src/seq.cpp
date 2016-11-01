0dedfaa1fdcf3ddabdde97ca6281cad5fb12112d
rmnorris <rmnorris@well.ox.ac.uk>
2016-11-01 15:11:03 +0000

    more work on minimizer_sketch

--- src/seq.cpp
+++ src/seq.cpp
@@ -32,19 +32,18 @@ void Seq::minimizer_sketch (uint32_t w, uint32_t k)
     if (seq.length()+1 < w+k) {//cout << "Sequence too short to sketch" << endl; 
 	return;}
 
-    // Store minimizers in vector until the end, so don't have to sort every time add a new element
-    deque<Minimizer*> v;
     // initializations
     string kmer;
     uint64_t kh, smallest;
     Minimizer *m;
+    Minimizer *m_previous;
 
     // for each window position
     for(uint32_t wpos=0; wpos <= seq.length()-w-k+1 ; ++wpos)
     {
 	//cout << "wpos: " << wpos << endl;
     	// if wpos==0 or the previous minimizer starts outside current window, calculate new mini from scratch
-        if((wpos == 0) or (v.front()->pos.start < wpos))
+        if((wpos == 0) or (m_previous->pos.start < wpos))
 	{
             smallest = std::numeric_limits<uint64_t>::max();
 	    // find the lex smallest kmer in the window
@@ -61,7 +60,8 @@ void Seq::minimizer_sketch (uint32_t w, uint32_t k)
 	        if (kh == smallest)
                 {
 		    m = new Minimizer(kh, wpos+i, wpos+i+k);
-		    v.push_front(m);
+		    sketch.insert(m);
+		    m_previous = m;
 	        }
 	    }
         } else {
@@ -72,21 +72,13 @@ void Seq::minimizer_sketch (uint32_t w, uint32_t k)
 	    if(kh <= smallest)
 	    {
 	        m = new Minimizer(kh, wpos+w-1, wpos+w-1+k);
-                pointer_values_equal<Minimizer> eq = { m };
-                if ( find_if(v.begin(), v.end(), eq) == v.end() )
-                {
-		    //cout << "Added minimizer " << *m << endl;
-                    v.push_front(m);
-                } else {
-                    delete m;
-                }
+		sketch.insert(m);
+		m_previous = m;
        	    }
             smallest = min(smallest, kh);
 	}
     }
     
-    // Now add the vector of minimizers to the sketch set
-    sketch.insert(v.begin(), v.end());
     return;
 }
 
