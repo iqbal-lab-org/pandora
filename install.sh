5124f6c49a1c970fede445cbd8569c9f28eeb052
rmnorris <rmnorris@well.ox.ac.uk>
2016-11-02 21:05:38 +0000

    want to merge dev branch, one last different to push first

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 mkdir build
 cd build
-cmake .. -DCMAKE_CXX_COMPILER=g++ -DCMAKE_CC_COMPILER=gcc
+cmake .. -DCMAKE_CXX_COMPILER=g++ -DCMAKE_CC_COMPILER=gcc -DCMAKE_BUILD_TYPE=Debug
 cmake --build .
 ctest -VV
+cd ..
