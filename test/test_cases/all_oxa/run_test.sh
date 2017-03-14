74d160e2a718e91ba20350685c586d18124f9865
rmnorris <rmnorris@well.ox.ac.uk>
2017-03-14 18:41:53 +0000

    last commit before remove old version of map method, keeping the inference of max path from kmergraph only

--- test/test_cases/all_oxa/run_test.sh
+++ test/test_cases/all_oxa/run_test.sh
@@ -1,20 +1,21 @@
 k=$1
-mkdir k$k
-../../../build/pandora index oxa_kmeans_k15_ordered_prg.fasta -k $k
+w=$2
+mkdir k$k.w$w
+../../../build/pandora index oxa_kmeans_k15_ordered_prg.fasta -k $k -w $w
 
 for i in {2..532..2}
     do
 	echo $i
 	#head -n $i /data2/users/rachel/projects/reference_graphs/data/gram_neg_genefamilies/oxa/oxa.fasta | tail -n 2 &> oxa_read_$i.fasta
-	../../../build/pandora map -p oxa_kmeans_k15_ordered_prg.fasta -r oxa_read_$i.fasta -o k$k/$i. -w 1 -k $k -m 500 -e 0.0001 &> k$k/$i.log
-	echo -e "$(head -1 oxa_read_$i.fasta)" &>> k$k/results.txt
-	echo -e "$(head -2 oxa_read_$i.fasta | tail -n 1)\n$(head -2 k$k/$i._oxa_mlp.fasta | tail -n 1)" | ~/apps/cortex/scripts/analyse_variants/needleman_wunsch/needleman_wunsch --stdin --zam &>> k$k/results.txt
+	../../../build/pandora map -p oxa_kmeans_k15_ordered_prg.fasta -r oxa_read_$i.fasta -o k$k.w$w/$i. -w $w -k $k -m 500 -e 0.0001 &> k$k/$i.log
+	echo -e "$(head -1 oxa_read_$i.fasta)" &>> k$k.w$w/results.txt
+	echo -e "$(head -2 oxa_read_$i.fasta | tail -n 1)\n$(head -2 k$k.w$w/$i._oxa_mlp.fasta | tail -n 1)" | ~/apps/cortex/scripts/analyse_variants/needleman_wunsch/needleman_wunsch --stdin --zam &>> k$k.w$w/results.txt
 
-	echo -e "$(head -1 oxa_read_$i.fasta)" &>> k$k/new_results.txt
-        echo -e "$(head -2 oxa_read_$i.fasta | tail -n 1)\n$(head -2 k$k/$i._oxa_kmlp.fasta | tail -n 1)" | ~/apps/cortex/scripts/analyse_variants/needleman_wunsch/needleman_wunsch --stdin --zam &>> k$k/new_results.txt
+	echo -e "$(head -1 oxa_read_$i.fasta)" &>> k$k.w$w/new_results.txt
+        echo -e "$(head -2 oxa_read_$i.fasta | tail -n 1)\n$(head -2 k$k.w$w/$i._oxa_kmlp.fasta | tail -n 1)" | ~/apps/cortex/scripts/analyse_variants/needleman_wunsch/needleman_wunsch --stdin --zam &>> k$k.w$w/new_results.txt
 
-	echo -e "$(head -1 oxa_read_$i.fasta)" &>> k$k/diff_results.txt
-        echo -e "$(head -2 k$k/$i._oxa_mlp.fasta | tail -n 1)\n$(head -2 k$k/$i._oxa_kmlp.fasta | tail -n 1)" | ~/apps/cortex/scripts/analyse_variants/needleman_wunsch/needleman_wunsch --stdin --zam &>> k$k/diff_results.txt
+	echo -e "$(head -1 oxa_read_$i.fasta)" &>> k$k.w$w/diff_results.txt
+        echo -e "$(head -2 k$k.w$w/$i._oxa_mlp.fasta | tail -n 1)\n$(head -2 k$k/$i._oxa_kmlp.fasta | tail -n 1)" | ~/apps/cortex/scripts/analyse_variants/needleman_wunsch/needleman_wunsch --stdin --zam &>> k$k.w$w/diff_results.txt
     done
 #for i in {2..532..2}
 #    do 
