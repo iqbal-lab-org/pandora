2688b73e5362c746b6b4dffb5058264dbc3363c5
rmnorris <rmnorris@well.ox.ac.uk>
2017-06-16 17:48:17 +0100

    just fyi changed test slightly, mostly in terms of commenting out bits

--- test/test_cases/oxa/run_test.sh
+++ test/test_cases/oxa/run_test.sh
@@ -2,12 +2,12 @@ prg=oxa_aligned_kmeans_k15_orientated.fasta
 
 #bash ../../../header.sh &> test_index.log
 #../../../build/pandora index $prg -w 5 &>> test_index.log
-#bash ../../../header.sh &> test_oxa9_5.15.500.log
-#../../../build/pandora map -p $prg -r oxa9_read.fasta -o test_oxa9_5.15.500 -m 500 -e 0.0001 -w 5 &>> test_oxa9_5.15.500.log
-#echo -e "$(head -2 oxa9_read.fasta | tail -n 1)\n$(head -2 test_oxa9_5.15.500_oxa_kmlp.fasta | tail -n 1)" | ~/apps/cortex/scripts/analyse_variants/needleman_wunsch/needleman_wunsch --stdin --zam &> results.txt
+bash ../../../header.sh &> test_oxa9_5.15.500.log
+../../../build/pandora map -p $prg -r oxa9_read.fasta -o test_oxa9_5.15.500 -m 500 -e 0.0001 -w 5 &>> test_oxa9_5.15.500.log
+echo -e "$(head -2 oxa9_read.fasta | tail -n 1)\n$(head -2 test_oxa9_5.15.500_oxa_kmlp.fasta | tail -n 1)" | ~/apps/cortex/scripts/analyse_variants/needleman_wunsch/needleman_wunsch --stdin --zam &> results.txt
 #bash ../../../header.sh &> test_JR_5.15.500.log
 #../../../build/pandora map -p $prg -r JR_FAA63668_14102015_kpne_CAV1596_reads.fasta -o test_JR_5.15.500 -m 500 -w 5 &>> test_JR_5.15.500.log
 #echo -e "$(head -2 oxa9_read.fasta | tail -n 1)\n$(head -2 test_JR_5.15.500_oxa_kmlp.fasta | tail -n 1)" | ~/apps/cortex/scripts/analyse_variants/needleman_wunsch/needleman_wunsch --stdin --zam &>> results.txt
-bash ../../../header.sh &> test_all2d_JR_5.15.500.log
-perf record ../../../build/pandora map -p $prg -r JR_FAA63668_14102015_kpne_CAV1596_all2d_reads.fasta -o test_all2d_JR_5.15.500 -m 500 -w 5 &>> test_all2d_JR_5.15.500.log
-echo -e "$(head -2 oxa9_read.fasta | tail -n 1)\n$(head -2 test_all2d_JR_5.15.500_oxa_kmlp.fasta | tail -n 1)" | ~/apps/cortex/scripts/analyse_variants/needleman_wunsch/needleman_wunsch --stdin --zam &>> results.txt
+#bash ../../../header.sh &> test_all2d_JR_5.15.500.log
+#perf record ../../../build/pandora map -p $prg -r JR_FAA63668_14102015_kpne_CAV1596_all2d_reads.fasta -o test_all2d_JR_5.15.500 -m 500 -w 5 &>> test_all2d_JR_5.15.500.log
+#echo -e "$(head -2 oxa9_read.fasta | tail -n 1)\n$(head -2 test_all2d_JR_5.15.500_oxa_kmlp.fasta | tail -n 1)" | ~/apps/cortex/scripts/analyse_variants/needleman_wunsch/needleman_wunsch --stdin --zam &>> results.txt
