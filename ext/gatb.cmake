128c6d577ce270b49e3531a861c87f4f58c26db0
Robyn Ffrancon <rffrancon@gmail.com>
2018-04-26 14:33:58 +0100

    boost dependancy made external; GATB library added; example tmp unit test runs correctly

--- ext/gatb.cmake
+++ ext/gatb.cmake
@@ -1,21 +1,30 @@
 include(ExternalProject)
 
-execute_process(COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/ext)
+execute_process(COMMAND mkdir -p ${CMAKE_BINARY_DIR}/ext)
+execute_process(COMMAND mkdir -p ${CMAKE_BINARY_DIR}/bin)
+execute_process(COMMAND mkdir -p ${CMAKE_BINARY_DIR}/lib)
+execute_process(COMMAND mkdir -p ${CMAKE_BINARY_DIR}/include)
+
+set(GATB_DIR ${CMAKE_BINARY_DIR}/ext/gatb-core-1.4.1/gatb-core)
+set(GATB_BUILD_DIR ${GATB_DIR}/build)
+execute_process(COMMAND mkdir -p ${GATB_BUILD_DIR})
 
 ExternalProject_Add(gatb
         DOWNLOAD_COMMAND  wget https://github.com/GATB/gatb-core/archive/v1.4.1.tar.gz --timestamping -O gatb.tar.gz
-        DOWNLOAD_DIR      "${CMAKE_CURRENT_BINARY_DIR}/ext"
+        DOWNLOAD_DIR      "${CMAKE_BINARY_DIR}/ext"
         CONFIGURE_COMMAND ""
-        BUILD_COMMAND     bash -c "cd ${CMAKE_CURRENT_BINARY_DIR}/ext/gatb-core-1.4.1/gatb-core && mkdir -p build && cd build && cmake .. && make"
+        BUILD_COMMAND     bash -c "cd ${GATB_BUILD_DIR} && cmake ${GATB_DIR} && make"
         INSTALL_COMMAND   ""
         TEST_COMMAND      "")
 
 ExternalProject_Add_Step(gatb extract_tar
-        COMMAND tar -xvzf ${CMAKE_CURRENT_BINARY_DIR}/ext/gatb.tar.gz -C ${CMAKE_CURRENT_BINARY_DIR}/ext
+        COMMAND tar -xvzf ${CMAKE_BINARY_DIR}/ext/gatb.tar.gz -C ${CMAKE_BINARY_DIR}/ext
         DEPENDEES download
         DEPENDERS configure)
 
-include_directories(${CMAKE_CURRENT_BINARY_DIR}/ext/gatb-core-1.4.1/gatb-core/src
-        ${CMAKE_CURRENT_BINARY_DIR}/ext/gatb-core-1.4.1/gatb-core/build/include)
-
-link_directories(${CMAKE_CURRENT_BINARY_DIR}/ext/gatb-core-1.4.1/gatb-core/build/lib)
+ExternalProject_Add_Step(gatb copy
+        COMMAND bash -c "cp -r ${GATB_BUILD_DIR}/bin/* ${CMAKE_BINARY_DIR}/bin/"
+        COMMAND bash -c "cp -r ${GATB_BUILD_DIR}/lib/* ${CMAKE_BINARY_DIR}/lib/"
+        COMMAND bash -c "cp -r ${GATB_BUILD_DIR}/include/* ${CMAKE_BINARY_DIR}/include/"
+        COMMAND bash -c "cp -r ${GATB_DIR}/src/gatb/* ${CMAKE_BINARY_DIR}/include/gatb/"
+        DEPENDEES build)
