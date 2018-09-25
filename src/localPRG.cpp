3e8082592237204cfd8e5d1acae1a804f23cc5b0
Rachel Norris <rmnorris@well.ox.ac.uk>
2018-09-25 17:58:41 +0100

    add extra asserts

--- src/localPRG.cpp
+++ src/localPRG.cpp
@@ -1235,6 +1235,8 @@ void LocalPRG::add_sample_covgs_to_vcf(VCF &vcf,
         vector<string>::iterator sample_it = find(vcf.samples.begin(), vcf.samples.end(), sample_name);
         assert(sample_it != vcf.samples.end());
         auto sample_index = distance(vcf.samples.begin(), sample_it);
+        assert(sample_index != vcf.samples.end());
+        assert(record.samples.size() > sample_index);
 
         record.samples[sample_index]["MEAN_FWD_COVG"] = {};
         record.samples[sample_index]["MEAN_REV_COVG"] = {};
