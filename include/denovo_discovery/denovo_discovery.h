4b7ecabef14dd7870c0b84471d12a0e7e5f82fb7
Michael Hall <mbhall88@gmail.com>
2018-10-19 14:54:33 +0100

    set default params for find candidates function

--- include/denovo_discovery/denovo_discovery.h
+++ include/denovo_discovery/denovo_discovery.h
@@ -11,9 +11,9 @@ namespace denovo_discovery {
     void find_candidates(const std::set <std::pair<ReadCoordinate, GeneIntervalInfo>> &pangraph_coordinate_pairs,
                          const std::string &readfilepath,
                          const fs::path &output_directory,
-                         const uint32_t &local_assembly_kmer_size = 0,
-                         const uint32_t &kmer_attempts_count = 0,
-                         const uint32_t &max_path_length = 0,
+                         const uint32_t &local_assembly_kmer_size = g_local_assembly_kmer_size,
+                         const uint32_t &kmer_attempts_count = KMERS_TO_TRY,
+                         const uint32_t &max_path_length = g_max_length,
                          const uint32_t &padding_size = 0);
 }
 
