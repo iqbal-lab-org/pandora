4e62ce6d3882b165c1de69892b850a6bf326271d
Michael Hall <mbhall88@gmail.com>
2018-10-23 13:20:12 +0100

    add default padding_size and remove argument from find candidates

--- include/denovo_discovery/denovo_discovery.h
+++ include/denovo_discovery/denovo_discovery.h
@@ -13,14 +13,10 @@ namespace fs = boost::filesystem;
 namespace denovo_discovery {
     void find_candidates(
             const std::set<std::pair<ReadCoordinate, GeneIntervalInfo>> &pangraph_coordinate_pairs,
-            const std::string &readfilepath,
-            const fs::path &output_directory,
-            const double &error_rate,
-            const uint32_t &local_assembly_kmer_size = g_local_assembly_kmer_size,
-            const uint32_t &kmer_attempts_count = g_kmer_attempts_count,
-            const uint32_t &padding_size = 0);
+            const std::string &readfilepath, const fs::path &output_directory, const double &error_rate,
+            const uint32_t &local_assembly_kmer_size, const uint32_t &kmer_attempts_count);
 
-    double calculate_kmer_coverage(const uint32_t &read_covg, const uint32_t &ref_length, const uint32_t k,
+    double calculate_kmer_coverage(const uint32_t &read_covg, const uint32_t &ref_length, const uint32_t &k,
                                    const double &error_rate);
 }
 
--- src/map_main.cpp
+++ src/map_main.cpp
@@ -315,7 +315,7 @@ int pandora_map(int argc, char *argv[]) {
         denovo_discovery::find_candidates(pangraph_coordinate_pairs,
                                           readfile,
                                           fs::path(outdir),
-                                          e_rate, g_local_assembly_kmer_size, g_kmer_attempts_count, 11);
+                                          e_rate, g_local_assembly_kmer_size, g_kmer_attempts_count);
     }
 
     if (output_mapped_read_fa)
