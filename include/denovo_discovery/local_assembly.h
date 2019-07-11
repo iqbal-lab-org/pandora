c787c29939754ac40d99135a219f62b17296175a
leandro <leandro@ebi.ac.uk>
2019-07-11 01:06:45 +0100

    Increasing the parameters back to the previous values

--- include/denovo_discovery/local_assembly.h
+++ include/denovo_discovery/local_assembly.h
@@ -25,7 +25,7 @@ using BfsDistanceMap = std::map<std::string, uint32_t>;
 using DenovoPaths = std::vector<std::string>;
 
 constexpr float COVG_SCALING_FACTOR { 0.1 };
-constexpr auto MAX_NUMBER_CANDIDATE_PATHS { 25 };
+constexpr auto MAX_NUMBER_CANDIDATE_PATHS { 50 };
 
 
 class LocalAssemblyGraph : public Graph {
