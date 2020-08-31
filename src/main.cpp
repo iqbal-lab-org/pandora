4460eae803078dc4bc2badf68247f05b41176619
Michael Hall <michael@mbh.sh>
2020-08-31 17:29:08 +1000

    WIP: create CLI for pandora map

--- src/main.cpp
+++ src/main.cpp
@@ -56,28 +56,28 @@ int main(int argc, char* argv[])
 
     return 0;
 
-    int ret;
-    if (argc < 2)
-        return usage();
-    if (strcmp(argv[1], "index") == 0)
-        ret = pandora_index(argc - 1, argv + 1);
-    else if (strcmp(argv[1], "walk") == 0)
-        ret = pandora_walk(argc - 1, argv + 1);
-    else if (strcmp(argv[1], "map") == 0)
-        ret = pandora_map(argc - 1, argv + 1);
-    else if (strcmp(argv[1], "compare") == 0)
-        ret = pandora_compare(argc - 1, argv + 1);
-    else if (strcmp(argv[1], "check_kmergraph") == 0)
-        ret = pandora_check_kmergraph(argc - 1, argv + 1);
-    else if (strcmp(argv[1], "get_vcf_ref") == 0)
-        ret = pandora_get_vcf_ref(argc - 1, argv + 1);
-    else if (strcmp(argv[1], "random_path") == 0)
-        ret = pandora_random_path(argc - 1, argv + 1);
-    else if (strcmp(argv[1], "merge_index") == 0)
-        ret = pandora_merge_index(argc - 1, argv + 1);
-    else {
-        fprintf(stderr, "[main] unrecognized command '%s'\n", argv[1]);
-        return 1;
-    }
-    return ret;
+//    int ret;
+//    if (argc < 2)
+//        return usage();
+//    if (strcmp(argv[1], "index") == 0)
+//        ret = pandora_index(argc - 1, argv + 1);
+//    else if (strcmp(argv[1], "walk") == 0)
+//        ret = pandora_walk(argc - 1, argv + 1);
+//    else if (strcmp(argv[1], "map") == 0)
+//        ret = pandora_map(argc - 1, argv + 1);
+//    else if (strcmp(argv[1], "compare") == 0)
+//        ret = pandora_compare(argc - 1, argv + 1);
+//    else if (strcmp(argv[1], "check_kmergraph") == 0)
+//        ret = pandora_check_kmergraph(argc - 1, argv + 1);
+//    else if (strcmp(argv[1], "get_vcf_ref") == 0)
+//        ret = pandora_get_vcf_ref(argc - 1, argv + 1);
+//    else if (strcmp(argv[1], "random_path") == 0)
+//        ret = pandora_random_path(argc - 1, argv + 1);
+//    else if (strcmp(argv[1], "merge_index") == 0)
+//        ret = pandora_merge_index(argc - 1, argv + 1);
+//    else {
+//        fprintf(stderr, "[main] unrecognized command '%s'\n", argv[1]);
+//        return 1;
+//    }
+//    return ret;
 }
