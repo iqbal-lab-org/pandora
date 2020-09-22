a9508a55edf38562ef8888c7b433d3cbe73b0601
Michael Hall <michael@mbh.sh>
2020-09-22 17:25:01 +1000

    merge candidate regions [#228]

--- include/interval.h
+++ include/interval.h
@@ -3,14 +3,18 @@
 
 #include <cstdint>
 #include <iostream>
+#include <vector>
+#include <limits>
+#include <cassert>
+#include <algorithm>
 
 struct Interval {
     uint32_t start;
-    uint32_t length;
-    // in pilot, longest prg was 208,562 characters long
+    uint32_t length; // in pilot, longest prg was 208,562 characters long
 
     Interval(uint32_t = 0, uint32_t = 0);
 
+    // non-inclusive
     uint32_t get_end() const;
 
     friend std::ostream& operator<<(std::ostream& out, const Interval& i);
@@ -25,5 +29,9 @@ struct Interval {
 
     bool empty() const;
 };
+
+// Merge intervals within dist of each other. Changes the vector inplace.
+// Time complexity: O(N log(N)) Sort the vector, then a single linear pass through
+void merge_intervals_within(std::vector<Interval>&, uint32_t);
 
 #endif
--- test/interval_test.cpp
+++ test/interval_test.cpp
@@ -1,5 +1,5 @@
 #include "gtest/gtest.h"
-#include <stdint.h>
+#include <cstdint>
 #include <iostream>
 #include "interval.h"
 
@@ -121,4 +121,188 @@ TEST(intervalEmptyTest, nonEmptyIntervalReturnsFalse)
     const Interval non_empty_interval { 1, 4 };
 
     EXPECT_FALSE(non_empty_interval.empty());
+}
+
+class MergeIntervalsTest : public ::testing::Test {
+protected:
+    void SetUp() override
+    {
+        single_.insert(single_.end(), { Interval(0, 1) });
+        unsorted_disjoint_.insert(
+            unsorted_disjoint_.end(), { Interval(4, 6), Interval(0, 2) });
+        contained_.insert(contained_.end(), { Interval(0, 10), Interval(4, 6) });
+        overlap_.insert(overlap_.end(), { Interval(0, 3), Interval(2, 5) });
+        overlap_and_disjoint_.insert(overlap_and_disjoint_.end(),
+            { Interval(0, 3), Interval(2, 5), Interval(7, 10) });
+        disjoint_overlap_disjoint_.insert(disjoint_overlap_disjoint_.end(),
+            { Interval(0, 3), Interval(6, 9), Interval(8, 11), Interval(16, 20),
+                Interval(30, 34) });
+    }
+    std::vector<Interval> empty_;
+    std::vector<Interval> single_;
+    std::vector<Interval> unsorted_disjoint_;
+    std::vector<Interval> contained_;
+    std::vector<Interval> overlap_;
+    std::vector<Interval> overlap_and_disjoint_;
+    std::vector<Interval> disjoint_overlap_disjoint_;
+};
+
+TEST_F(MergeIntervalsTest, HandlesEmptyInput)
+{
+    const auto dist { 0 };
+
+    merge_intervals_within(empty_, dist);
+    const auto actual { empty_ };
+    const std::vector<Interval> expected;
+
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(MergeIntervalsTest, SingleElementDoesNothing)
+{
+    const auto dist { 0 };
+
+    const auto expected = single_;
+    merge_intervals_within(single_, dist);
+    const auto actual = single_;
+
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(MergeIntervalsTest, UnsortedDisjointIntervalsReturnsSortedIntervals)
+{
+    const auto dist { 0 };
+
+    std::vector<Interval> expected = unsorted_disjoint_;
+    std::sort(expected.begin(), expected.end());
+    merge_intervals_within(unsorted_disjoint_, dist);
+    const auto actual = unsorted_disjoint_;
+
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(MergeIntervalsTest, DisjointWithDistEdgeCase)
+{
+    const auto dist { 2 };
+
+    std::vector<Interval> expected = unsorted_disjoint_;
+    std::sort(expected.begin(), expected.end());
+    merge_intervals_within(unsorted_disjoint_, dist);
+    const auto actual = unsorted_disjoint_;
+
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(MergeIntervalsTest, DisjointWithDistCausingOverlap)
+{
+    const auto dist { 3 };
+
+    merge_intervals_within(unsorted_disjoint_, dist);
+    const auto actual = unsorted_disjoint_;
+    const std::vector<Interval> expected = { Interval(0, 6) };
+
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(MergeIntervalsTest, ContainedIntervalIsMerged)
+{
+    const auto dist { 0 };
+
+    merge_intervals_within(contained_, dist);
+    const auto actual = contained_;
+    const std::vector<Interval> expected = { Interval(0, 10) };
+
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(MergeIntervalsTest, OverlappingIsMerged)
+{
+    const auto dist { 0 };
+
+    merge_intervals_within(overlap_, dist);
+    const auto actual = overlap_;
+    const std::vector<Interval> expected = { Interval(0, 5) };
+
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(MergeIntervalsTest, OverlapAndDisjointNoDistOnlyOverlapMerged)
+{
+    const auto dist { 0 };
+
+    merge_intervals_within(overlap_and_disjoint_, dist);
+    const auto actual = overlap_and_disjoint_;
+    const std::vector<Interval> expected = { Interval(0, 5), Interval(7, 10) };
+
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(MergeIntervalsTest, OverlapAndDisjointEdgeDistOnlyOverlapMerged)
+{
+    const auto dist { 2 };
+
+    merge_intervals_within(overlap_and_disjoint_, dist);
+    const auto actual = overlap_and_disjoint_;
+    const std::vector<Interval> expected = { Interval(0, 5), Interval(7, 10) };
+
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(MergeIntervalsTest, OverlapAndDisjointOverlapDistMergeAll)
+{
+    const auto dist { 3 };
+
+    merge_intervals_within(overlap_and_disjoint_, dist);
+    const auto actual = overlap_and_disjoint_;
+    const std::vector<Interval> expected = { Interval(0, 10) };
+
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(MergeIntervalsTest, ManyDisjointAndOverlappingNoDistOnlyMergeOverlaps)
+{
+    const auto dist { 0 };
+
+    merge_intervals_within(disjoint_overlap_disjoint_, dist);
+    const auto actual = disjoint_overlap_disjoint_;
+    const std::vector<Interval> expected
+        = { Interval(0, 3), Interval(6, 11), Interval(16, 20), Interval(30, 34) };
+
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(
+    MergeIntervalsTest, ManyDisjointAndOverlappingSmallDistMergeOverlapsAndOneDisjoint)
+{
+    const auto dist { 5 };
+
+    merge_intervals_within(disjoint_overlap_disjoint_, dist);
+    const auto actual = disjoint_overlap_disjoint_;
+    const std::vector<Interval> expected
+        = { Interval(0, 11), Interval(16, 20), Interval(30, 34) };
+
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(
+    MergeIntervalsTest, ManyDisjointAndOverlappingMediumDistMergeOverlapsAndTwoDisjoint)
+{
+    const auto dist { 9 };
+
+    merge_intervals_within(disjoint_overlap_disjoint_, dist);
+    const auto actual = disjoint_overlap_disjoint_;
+    const std::vector<Interval> expected = { Interval(0, 20), Interval(30, 34) };
+
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(MergeIntervalsTest, ManyDisjointAndOverlappingLargeDistMergeAll)
+{
+    const auto dist { 100 };
+
+    merge_intervals_within(disjoint_overlap_disjoint_, dist);
+    const auto actual = disjoint_overlap_disjoint_;
+    const std::vector<Interval> expected = { Interval(0, 34) };
+
+    EXPECT_EQ(actual, expected);
 }
