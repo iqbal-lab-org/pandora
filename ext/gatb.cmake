3171550a63d402d41d0729383ab835539b2f906e
Michael Hall <michael@mbh.sh>
2020-10-21 17:43:03 +1000

    exclude nonessential GATB files and limit kszie

--- ext/gatb.cmake
+++ ext/gatb.cmake
@@ -1,30 +1,15 @@
 include(ExternalProject)
 
-execute_process(COMMAND mkdir -p ${CMAKE_BINARY_DIR}/ext)
-execute_process(COMMAND mkdir -p ${CMAKE_BINARY_DIR}/bin)
-execute_process(COMMAND mkdir -p ${CMAKE_BINARY_DIR}/lib)
-execute_process(COMMAND mkdir -p ${CMAKE_BINARY_DIR}/include)
-
-set(GATB_DIR ${CMAKE_BINARY_DIR}/ext/gatb-core-1.4.1/gatb-core)
-set(GATB_BUILD_DIR ${GATB_DIR}/build)
-execute_process(COMMAND mkdir -p ${GATB_BUILD_DIR})
+# We don't want to install some GATB-CORE artifacts
+SET (GATB_CORE_EXCLUDE_TOOLS     1)
+SET (GATB_CORE_EXCLUDE_TESTS     1)
+SET (GATB_CORE_INCLUDE_EXAMPLES  1)
 
 ExternalProject_Add(gatb
-        DOWNLOAD_COMMAND  wget https://github.com/GATB/gatb-core/archive/v1.4.1.tar.gz --timestamping -O gatb.tar.gz
-        DOWNLOAD_DIR      "${CMAKE_BINARY_DIR}/ext"
-        CONFIGURE_COMMAND ""
-        BUILD_COMMAND     bash -c "cd ${GATB_BUILD_DIR} && cmake -D CMAKE_C_COMPILER=${CMAKE_C_COMPILER} -D CMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} ${GATB_DIR} && make"
-        INSTALL_COMMAND   ""
-        TEST_COMMAND      "")
+        GIT_REPOSITORY https://github.com/GATB/gatb-core.git
+        GIT_TAG "v1.4.1"
+        PREFIX "${CMAKE_CURRENT_BINARY_DIR}/gatb"
+        SOURCE_DIR "${CMAKE_CURRENT_BINARY_DIR}/gatb/src/gatb/gatb-core/gatb-core"
+        CMAKE_ARGS -DKSIZE_LIST=32
+        INSTALL_COMMAND "")
 
-ExternalProject_Add_Step(gatb extract_tar
-        COMMAND tar -xvzf ${CMAKE_BINARY_DIR}/ext/gatb.tar.gz -C ${CMAKE_BINARY_DIR}/ext
-        DEPENDEES download
-        DEPENDERS configure)
-
-ExternalProject_Add_Step(gatb copy
-        COMMAND bash -c "cp -r ${GATB_BUILD_DIR}/bin/* ${CMAKE_BINARY_DIR}/bin/"
-        COMMAND bash -c "cp -r ${GATB_BUILD_DIR}/lib/* ${CMAKE_BINARY_DIR}/lib/"
-        COMMAND bash -c "cp -r ${GATB_BUILD_DIR}/include/* ${CMAKE_BINARY_DIR}/include/"
-        COMMAND bash -c "cp -r ${GATB_DIR}/src/gatb/* ${CMAKE_BINARY_DIR}/include/gatb/"
-        DEPENDEES build)
