fa1cc55b00e278056042b9ce780611c08157dce6
Leandro Ishi <leandro.ishi.lima@gmail.com>
2020-12-18 00:42:44 -0300

    Removing requirement to have system boost installed

--- ext/boost.cmake
+++ ext/boost.cmake
@@ -1,17 +1,18 @@
 include(ExternalProject)
 
-execute_process(COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/ext/boost_1_65_1)
+execute_process(COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/ext/boost_1_62_0)
 
 ExternalProject_Add(boost
-        DOWNLOAD_COMMAND  wget https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.gz --timestamping
+        DOWNLOAD_COMMAND  wget https://sourceforge.net/projects/boost/files/boost/1.62.0/boost_1_62_0.tar.gz --timestamping
         DOWNLOAD_DIR      "${CMAKE_CURRENT_BINARY_DIR}/ext"
-        CONFIGURE_COMMAND bash -c "cd ${CMAKE_CURRENT_BINARY_DIR}/ext/boost_1_65_1 && ./bootstrap.sh --with-libraries=filesystem,system,iostreams --prefix=${CMAKE_CURRENT_BINARY_DIR}"
-        BUILD_COMMAND     bash -c "cd ${CMAKE_CURRENT_BINARY_DIR}/ext/boost_1_65_1 && ./bjam install link=static"
-        INSTALL_COMMAND   ""
-        TEST_COMMAND      "")
+        CONFIGURE_COMMAND bash -c "cd ${CMAKE_CURRENT_BINARY_DIR}/ext/boost_1_62_0 && ./bootstrap.sh --with-libraries=system,filesystem,iostreams,log,thread,date_time --prefix=${CMAKE_CURRENT_BINARY_DIR}/boost"
+        BUILD_COMMAND     ""
+        INSTALL_COMMAND   bash -c "cd ${CMAKE_CURRENT_BINARY_DIR}/ext/boost_1_62_0 && ./b2 install link=static"
+        TEST_COMMAND      ""
+        )
 
 ExternalProject_Add_Step(boost extract_tar
-        COMMAND tar -xvzf ${CMAKE_CURRENT_BINARY_DIR}/ext/boost_1_65_1.tar.gz -C ${CMAKE_CURRENT_BINARY_DIR}/ext
+        COMMAND tar -xvzf ${CMAKE_CURRENT_BINARY_DIR}/ext/boost_1_62_0.tar.gz -C ${CMAKE_CURRENT_BINARY_DIR}/ext
         DEPENDEES download
-        DEPENDERS configure)
-
+        DEPENDERS configure
+        )
