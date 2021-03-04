a36c75f2af07dc99e88be294f19b23955c61f578
Leandro Ishi <leandro.ishi.lima@gmail.com>
2021-03-04 15:48:40 +0000

    Updating example to use the precompiled binary

--- example/run_pandora_nodenovo.sh
+++ example/run_pandora_nodenovo.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
 set -eu
-pandora_command="pandora index prgs/toy_prg.fa && pandora compare --genotype -o output_toy_example_no_denovo prgs/toy_prg.fa reads/read_index.tsv"
-singularity exec docker://rmcolq/pandora:latest bash -c "${pandora_command}"
+
+# configs
+pandora_URL="https://www.dropbox.com/s/3ofko0q5fcec5xe/pandora-linux-precompiled-v0.8.0-alpha?dl=1"
+pandora_executable="./pandora-linux-precompiled-v0.8.0-alpha"
+pandora_md5sum_file="./pandora-linux-precompiled-v0.8.0-alpha.md5sum.txt"
+
+
+if md5sum -c "${pandora_md5sum_file}"; then
+    # The MD5 sum match
+    echo "${pandora_executable} has correct MD5 sum, proceeding..."
+else
+    # The MD5 sum didn't match
+    echo "${pandora_executable} does not exist or does not have correct MD5 sum, downloading..."
+    wget "${pandora_URL}" -O "${pandora_executable}"
+    chmod +x "${pandora_executable}"
+fi
+
+"${pandora_executable}" index prgs/toy_prg.fa
+"${pandora_executable}" compare --genotype -o output_toy_example_no_denovo prgs/toy_prg.fa reads/read_index.tsv
+
