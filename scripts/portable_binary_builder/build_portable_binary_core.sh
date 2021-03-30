77cf03322666c9a9f2f72dc887fff852a0e75482
Leandro Ishi <leandro.ishi.lima@gmail.com>
2021-03-30 21:24:06 +0100

    Using musl instead of HBB to build the portable executable

--- scripts/portable_binary_builder/build_portable_binary_core.sh
+++ scripts/portable_binary_builder/build_portable_binary_core.sh
@@ -1,30 +1,14 @@
 #!/bin/bash
 
-set -e
-
-# Activate Holy Build Box environment.
-source /hbb_exe/activate
-
 set -eux
 
-# install packages needed to compile
-yum install wget git binutils-devel -y
-
 # compile pandora
-cd io
+cd /pandora
 mkdir build_portable_executable
 cd build_portable_executable
-cmake -DPRINT_STACKTRACE=True -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_EXE_LINKER_FLAGS="-static" -DCMAKE_BUILD_TYPE=Release ..
 make VERBOSE=1 -j 4
 ctest -VV
 
-# verify if the binary is portable
-set +e
-libcheck pandora
-set -e
-
-# print ldd output for us to check the dependencies also
-ldd pandora
-
 # copy binary to host filesystem
-cp pandora /io/pandora-linux-precompiled
+cp pandora /pandora/pandora-linux-precompiled
