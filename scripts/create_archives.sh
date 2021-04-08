69c88d715b47ffa3475f7f7b9db98fb48e0dd302
Leandro Ishi <leandro.ishi.lima@gmail.com>
2021-04-08 15:32:00 +0100

    Improving the create_archives script

--- scripts/create_archives.sh
+++ scripts/create_archives.sh
@@ -1,30 +1,54 @@
 #!/bin/bash
-# run from project root: scripts/create_archives.sh
+# run from project root: scripts/create_archives.sh <PANDORA_VERSION>
 # based on https://github.com/nzanepro/git-archive-submodules/blob/master/bin/git-archive-submodules.sh
 set -eu
 
-ARCHIVES_DIR="./archives"
+########################################################################################################################
+# argument parsing
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters."
+    echo "Usage: $0 <PANDORA_VERSION>"
+    echo "Example: $0 0.8.0"
+    exit 1
+fi
+PANDORA_VERSION="$1"
+########################################################################################################################
 
+########################################################################################################################
+# configs
+PANDORA_URL="https://github.com/rmcolq/pandora"
+ARCHIVES_DIR="./archives"
+########################################################################################################################
+
+########################################################################################################################
+# main script
+ARCHIVES_DIR=$(realpath "${ARCHIVES_DIR}")
 if [ -d "${ARCHIVES_DIR}" ]; then
   echo "Please remove ${ARCHIVES_DIR} before proceeding."
   exit 1
 fi
 
-TARMODULE=$(basename "$(git rev-parse --show-toplevel)")
-TARVERSION=$(git describe --tags --abbrev=0)
-TARPREFIX="${TARMODULE}-${TARVERSION}"
+mkdir -p "${ARCHIVES_DIR}"
+cd "${ARCHIVES_DIR}"
+
+TARPREFIX="pandora-${PANDORA_VERSION}"
+echo "Cloning ${TARPREFIX}"
+git clone --recursive --depth=1 --single-branch --branch "${PANDORA_VERSION}" "${PANDORA_URL}" "${PANDORA_VERSION}"
 
 echo "Creating tar archive..."
-mkdir -p archives
-git archive --prefix="${TARPREFIX}"/ -o archives/"${TARPREFIX}".tar "${TARVERSION}"
+cd "${PANDORA_VERSION}"
+git archive --prefix="${TARPREFIX}"/ -o "${ARCHIVES_DIR}/${TARPREFIX}.tar" "${PANDORA_VERSION}"
 git submodule foreach --recursive \
-  "git archive --prefix=${TARPREFIX}/\${displaypath}/ HEAD > ../../archives/tmp.tar && \
-  tar --concatenate --file=../../archives/${TARPREFIX}.tar ../../archives/tmp.tar" > /dev/null
-rm archives/tmp.tar
+  "git archive --prefix=${TARPREFIX}/\${displaypath}/ HEAD > ${ARCHIVES_DIR}/tmp.tar && \
+  tar --concatenate --file=${ARCHIVES_DIR}/${TARPREFIX}.tar ${ARCHIVES_DIR}/tmp.tar" > /dev/null
+rm "${ARCHIVES_DIR}/tmp.tar"
 
 echo "Compressing to tar.gz..."
-gzip -9 archives/"${TARPREFIX}".tar
+gzip -9 "${ARCHIVES_DIR}/${TARPREFIX}.tar"
 
 echo "Compressing to zip..."
-cd archives && tar xzf "${TARPREFIX}".tar.gz && \
-zip -r "${TARPREFIX}".zip "${TARPREFIX}" > /dev/null
+cd "${ARCHIVES_DIR}" && tar xzf "${TARPREFIX}.tar.gz" && \
+zip -r "${TARPREFIX}.zip" "${TARPREFIX}" > /dev/null
+
+echo "All done!"
+########################################################################################################################
