01c0931a53e9750ad3cff378dab62598a5850978
Leandro Ishi <leandro.ishi.lima@gmail.com>
2021-05-17 23:14:09 +0100

    Updating example with a self check

--- example/run_pandora.sh
+++ example/run_pandora.sh
@@ -20,20 +20,26 @@ download_tool "${make_prg_URL}" "${make_prg_executable}"
 
 echo "Running pandora without denovo..."
 echo "Running ${make_prg_executable} from_msa"
-"${make_prg_executable}" from_msa --input msas/ --output_prefix prgs/pangenome
+"${make_prg_executable}" from_msa --threads 1 --input msas/ --output_prefix out/prgs/pangenome
 echo "Running ${pandora_executable} index"
-"${pandora_executable}" index prgs/pangenome.prg.fa
+"${pandora_executable}" index --threads 1 out/prgs/pangenome.prg.fa
 echo "Running ${pandora_executable} compare"
-"${pandora_executable}" compare --genotype -o output_toy_example_no_denovo prgs/pangenome.prg.fa reads/read_index.tsv
+"${pandora_executable}" compare --threads 1 --genotype -o out/output_toy_example_no_denovo out/prgs/pangenome.prg.fa reads/read_index.tsv
 echo "Running pandora without denovo - done!"
 
 echo "Running pandora with denovo..."
 echo "Running ${pandora_executable} discover"
-"${pandora_executable}" discover --outdir pandora_discover_out prgs/pangenome.prg.fa reads/read_index.tsv
+"${pandora_executable}" discover --threads 1 --outdir out/pandora_discover_out out/prgs/pangenome.prg.fa reads/read_index.tsv
 echo "Running ${make_prg_executable} update"
-"${make_prg_executable}" update --update_DS prgs/pangenome.update_DS --denovo_paths pandora_discover_out/denovo_paths.txt --output_prefix updated_prgs/pangenome_updated
+"${make_prg_executable}" update --threads 1 --update_DS out/prgs/pangenome.update_DS --denovo_paths out/pandora_discover_out/denovo_paths.txt --output_prefix out/updated_prgs/pangenome_updated
 echo "Running ${pandora_executable} index on updated PRGs"
-"${pandora_executable}" index updated_prgs/pangenome_updated.prg.fa
+"${pandora_executable}" index --threads 1 out/updated_prgs/pangenome_updated.prg.fa
 echo "Running ${pandora_executable} compare"
-"${pandora_executable}" compare --genotype -o output_toy_example_with_denovo updated_prgs/pangenome_updated.prg.fa reads/read_index.tsv
+"${pandora_executable}" compare --threads 1 --genotype -o out/output_toy_example_with_denovo out/updated_prgs/pangenome_updated.prg.fa reads/read_index.tsv
 echo "Running pandora with denovo - done!"
+
+if diff -rq -I '##fileDate.*' out out_truth ; then
+    echo "Example run produced the expected result"
+else
+    echo "ERROR: Example run DID NOT produce the expected result"
+fi
