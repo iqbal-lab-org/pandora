b3bc7660ffac4fa4572122a16c7a44ef13827e54
Leandro Ishi <leandro.ishi.lima@gmail.com>
2022-02-25 15:58:12 +0000

    Removing unused parameters

--- include/denovo_discovery/discover_main.h
+++ include/denovo_discovery/discover_main.h
@@ -12,7 +12,6 @@
 #include "noise_filtering.h"
 #include "estimate_parameters.h"
 
-constexpr auto MAX_DENOVO_K { 32 };
 namespace fs = boost::filesystem;
 
 /// Collection of all options of discover subcommand.
@@ -33,14 +32,6 @@ struct DiscoverOptions {
     bool clean { false };
     bool binomial { false };
     uint32_t max_covg { 600 };
-    uint16_t denovo_kmer_size { 15 };
-    uint16_t max_insertion_size { 15 };
-    uint16_t min_covg_for_node_in_assembly_graph { 2 };
-    uint32_t min_candidate_covg { 3 };
-    uint32_t min_candidate_len { 1 };
-    uint32_t max_candidate_len { 30 };
-    int max_num_candidate_paths { 25 };
-    uint32_t merge_dist { 15 };
     uint32_t min_cluster_size { 10 };
     uint32_t max_num_kmers_to_avg { 100 };
     bool clean_dbg { false };
