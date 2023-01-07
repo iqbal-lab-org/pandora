cd337248f7e1d19b4d5fc34a81f831243f864b75
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-01-07 00:59:31 +0000

    Adapting random subcommand to new index interface

--- include/random_main.h
+++ include/random_main.h
@@ -13,7 +13,7 @@
 #include <boost/log/expressions.hpp>
 
 struct RandomOptions {
-    std::string prgfile;
+    fs::path pandora_index_file;
     bool compress { false };
     uint32_t num_paths { 1 };
     uint8_t verbosity { 0 };
